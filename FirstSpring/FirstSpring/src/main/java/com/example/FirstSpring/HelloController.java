77b360f399abb6a800ee5df8e947f2ddc894774f
Jenkibear <jenkinwork123@gmail.com>
2025-01-02 11:49:42 +1100

    crud

---
+++ FirstSpring/FirstSpring/src/main/java/com/example/FirstSpring/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.FirstSpring;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello/{name}")
+    public HelloResponse helloParam(@PathVariable String name){
+        return new HelloResponse("Hello, World"+ name);
+    }
+
+    @GetMapping("/hello")
+    public HelloResponse hello(){
+        return new HelloResponse("Hello, World");
+    }
+
+    @PostMapping("/hello")
+    public HelloResponse helloPost(@RequestBody String name){
+        return new HelloResponse("Hello" +name +"!");
+    }
+}
